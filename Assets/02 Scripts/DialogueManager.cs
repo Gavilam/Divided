d4473e14339e840c7b986e52fde29a1de7da9f39
d3val <devalt5008@gmail.com>
2024-05-28 11:56:19 -0600

    DialagueManager actualización
    - Agregado soporte para los datos de los documentos e información secundaria

--- Assets/02 Scripts/DialogueManager.cs
+++ Assets/02 Scripts/DialogueManager.cs
@@ -9,7 +9,9 @@ using UnityEngine.UIElements;
 public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager Instance;
-    public TextAsset file;
+    public TextAsset mainNarrativeFile;
+    public TextAsset docsContentFile;
+    public TextAsset secondaryInfoFile;
     [Header("UI elements")]
     [SerializeField] GameObject dialoguePanel;
     [SerializeField] TextMeshProUGUI speakerNameText;
@@ -20,13 +22,17 @@ public class DialogueManager : MonoBehaviour
     public bool isShowingDialogue = false;
     string tarjetDialogue = "";
     string currentSpeakerName;
-    List<string> dialoguesData = new List<string>();
+    List<string> mainNarrativeData = new List<string>();
+    List<string> docsData = new List<string>();
+    List<string> secondaryData = new List<string>();
 
     private void Awake()
     {
         Instance = this;
 
-        ReadData();
+        ReadData(mainNarrativeData, mainNarrativeFile);
+        ReadData(docsData, docsContentFile);
+        ReadData(secondaryData, secondaryInfoFile);
     }
 
     //Inicia la corrutina para monstrar un dialogo
@@ -76,10 +82,10 @@ public class DialogueManager : MonoBehaviour
         isShowingDialogue = false;
     }
 
-    void ReadData()
+    void ReadData(List<string> list, TextAsset fileSource)
     {
         //Se lee el contenido de las celdas.
-        string[] data = file.text.Split(new string[] { ",", "\n" }, System.StringSplitOptions.None);
+        string[] data = fileSource.text.Split(new string[] { ",", "\n" }, System.StringSplitOptions.None);
 
         //Se realiza una correccion en los datos leidos.
         string aux = "";
@@ -98,7 +104,7 @@ public class DialogueManager : MonoBehaviour
                 errorFounded = false;
                 aux = aux + "" + s;
                 aux = aux.Replace("\"", "");
-                dialoguesData.Add(aux);
+                list.Add(aux);
                 aux = "";
                 //Se usa para evitar un duplicado de informacion
                 flag = false;
@@ -112,19 +118,19 @@ public class DialogueManager : MonoBehaviour
             else
             {
                 if (flag)
-                    dialoguesData.Add(s);
+                    list.Add(s);
             }
         }
     }
 
     void SearchDialogue(string code)
     {
-        for (int i = 0; i < dialoguesData.Count; i++)
+        for (int i = 0; i < mainNarrativeData.Count; i++)
         {
-            if (dialoguesData[i] == code)
+            if (mainNarrativeData[i] == code)
             {
-                currentSpeakerName = dialoguesData[i + 1];
-                tarjetDialogue = dialoguesData[i + 2];
+                currentSpeakerName = mainNarrativeData[i + 1];
+                tarjetDialogue = mainNarrativeData[i + 2];
             }
         }
     }
