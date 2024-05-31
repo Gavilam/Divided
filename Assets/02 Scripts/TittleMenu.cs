c1788690db022e228f14a2724f4531eefa68c129
Eduardo Alvarez <90423330+d3val@users.noreply.github.com>
2024-05-31 16:28:56 -0600

    Tittle menu (#5)
    * tittle menu
    
    * Menu de inicio

---
+++ Assets/02 Scripts/TittleMenu.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TittleMenu : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer;
+    [Header("Settings elements")]
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider SFXSlider;
+    string settingsSaveFilePath;
+    SettingsData settingsData;
+
+    private void Awake()
+    {
+        settingsData = new SettingsData();
+        settingsSaveFilePath = Application.persistentDataPath + "/SettingsData.json";
+    }
+
+    private void Start()
+    {
+        LoadSettings();
+    }
+
+    public void LoadScene(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("Music_volume", volume);
+        audioMixer.GetFloat("Music_volume", out settingsData.musicVolume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFX_volume", volume);
+        audioMixer.GetFloat("SFX_volume", out settingsData.SFXVolume);
+    }
+
+    //Guarda los valores de saveSettingsData en un Json
+    public void SaveSettings()
+    {
+        string saveSettingsData = JsonUtility.ToJson(settingsData);
+        File.WriteAllText(settingsSaveFilePath, saveSettingsData);
+    }
+
+    //Le los datos de ajuste guardados y los aplica a los diverson componentes/elementos
+    void LoadSettings()
+    {
+        string loadSettingsData = File.ReadAllText(settingsSaveFilePath);
+        settingsData = JsonUtility.FromJson<SettingsData>(loadSettingsData);
+        audioMixer.SetFloat("SFX_volume", settingsData.SFXVolume);
+        audioMixer.SetFloat("Music_volume", settingsData.musicVolume);
+        musicSlider.value = settingsData.musicVolume;
+        SFXSlider.value = settingsData.SFXVolume;
+    }
+
+    //Clase contenedora de los datos de ajuste
+    public class SettingsData
+    {
+        public float musicVolume;
+        public float SFXVolume;
+    }
+}
